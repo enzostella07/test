3d8aa6f69dff20e8b351ad5d17b7fd25353ce971
Enzo Stella <enzo.stella07@gmail.com>
2023-05-24 01:15:36 -0300

    first

---
+++ src/app.js
@@ -0,0 +1,72 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import path from "path";
+import { Server } from "socket.io";
+import { petsRouter } from "./routes/pets.router.js";
+import { testSocketChatRouter } from "./routes/test.socket.chat.router.js";
+import { usersHtmlRouter } from "./routes/users.html.router.js";
+import { usersRouter } from "./routes/users.router.js";
+import { __dirname } from "./utils.js";
+
+const app = express();
+const port = 3000;
+
+const httpServer = app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
+
+const socketServer = new Server(httpServer);
+let msgs = [];
+//BACK
+socketServer.on("connection", (socket) => {
+  //BACK EMITE
+  /*socket.emit("msg_back_to_front", {
+    msg: Date.now() + "hola desde el back al socket",
+  });*/
+
+  //BACK RECIBE
+  socket.on("msg_front_to_back", (msg) => {
+    console.log(msg);
+    msgs.unshift(msg);
+    socketServer.emit("msg_back_to_front", msgs);
+  });
+
+  // socket.broadcast.emit("msg_back_to_todos_menos_socket"),
+  //   {
+  //     msg: "hola desde el back a todos menos el socket",
+  //   };
+
+  // socketServer.emit("msg_back_todos", { msg: "hola desde l back a todos" });
+
+  socket.on("msg_front_to_back", (data) => {
+    console.log(JSON.stringify(data));
+  });
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "handlebars");
+
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Rutas: API REST CON JSON
+app.use("/api/users", usersRouter);
+app.use("/api/pets", petsRouter);
+
+//Rutas: HTML RENDER SERVER SIDE
+app.use("/users", usersHtmlRouter);
+
+//Rutas: SOCKETS
+app.use("/test-chat", testSocketChatRouter);
+
+app.get("*", (req, res) => {
+  return res.status(404).json({
+    status: "error",
+    msg: "no encontrado",
+    data: {},
+  });
+});
---
+++ src/routes/test.socket.chat.router.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+export const testSocketChatRouter = express.Router();
+
+testSocketChatRouter.get("/", (req, res) => {
+    return res.render("test-chat", {})
+});
+
